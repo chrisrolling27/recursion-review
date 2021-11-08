68dbfec510e1b5f3adee517a56fced574cdd9f2e
ChrisRolling <chrisrolling27@gmail.com>
2021-11-08 13:27:30 -0800

    Sprint progress - almost finished with the first:

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
   // your code here
+  //document body if no default input
+  var node = node || document.body;
+
+  var outputArr = [];
+
+  console.log(node);
+
+  if (node.className === className) {
+    outputArr.push(node);
+    console.log(outputArr);
+  }
+  //iterate through all elements, check if they have a class name
+  var arrChild = node.children;
+  console.log(arrChild);
+
+  for (var i=0; i < arrChild.length; i++) {
+    console.log(i);
+    console.log(arrChild[i]);
+    getElementsByClassName(className, arrChild[i]);
+  }
+  return outputArr;
+
 };
+
